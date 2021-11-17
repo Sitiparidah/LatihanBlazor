d1f0884995f594e631995be1f36640a455140e9a
Sitiparidah <sitiparidahpei7@gmail.com>
2021-11-17 16:00:42 +0700

    First commit to cloned repo

--- obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,77 +13,77 @@ namespace LatihanBlazor.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\xampp\htdocs\PEMROGRAMANFRONTEND\test\UTS\Employees\_Imports.razor"
+#line 1 "C:\xampp\htdocs\PEMROGRAMANFRONTEND\test\Tugas 5\Employees\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\xampp\htdocs\PEMROGRAMANFRONTEND\test\UTS\Employees\_Imports.razor"
+#line 2 "C:\xampp\htdocs\PEMROGRAMANFRONTEND\test\Tugas 5\Employees\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\xampp\htdocs\PEMROGRAMANFRONTEND\test\UTS\Employees\_Imports.razor"
+#line 3 "C:\xampp\htdocs\PEMROGRAMANFRONTEND\test\Tugas 5\Employees\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\xampp\htdocs\PEMROGRAMANFRONTEND\test\UTS\Employees\_Imports.razor"
+#line 4 "C:\xampp\htdocs\PEMROGRAMANFRONTEND\test\Tugas 5\Employees\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\xampp\htdocs\PEMROGRAMANFRONTEND\test\UTS\Employees\_Imports.razor"
+#line 5 "C:\xampp\htdocs\PEMROGRAMANFRONTEND\test\Tugas 5\Employees\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\xampp\htdocs\PEMROGRAMANFRONTEND\test\UTS\Employees\_Imports.razor"
+#line 6 "C:\xampp\htdocs\PEMROGRAMANFRONTEND\test\Tugas 5\Employees\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\xampp\htdocs\PEMROGRAMANFRONTEND\test\UTS\Employees\_Imports.razor"
+#line 7 "C:\xampp\htdocs\PEMROGRAMANFRONTEND\test\Tugas 5\Employees\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\xampp\htdocs\PEMROGRAMANFRONTEND\test\UTS\Employees\_Imports.razor"
+#line 8 "C:\xampp\htdocs\PEMROGRAMANFRONTEND\test\Tugas 5\Employees\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\xampp\htdocs\PEMROGRAMANFRONTEND\test\UTS\Employees\_Imports.razor"
+#line 9 "C:\xampp\htdocs\PEMROGRAMANFRONTEND\test\Tugas 5\Employees\_Imports.razor"
 using LatihanBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\xampp\htdocs\PEMROGRAMANFRONTEND\test\UTS\Employees\_Imports.razor"
+#line 10 "C:\xampp\htdocs\PEMROGRAMANFRONTEND\test\Tugas 5\Employees\_Imports.razor"
 using LatihanBlazor.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\xampp\htdocs\PEMROGRAMANFRONTEND\test\UTS\Employees\_Imports.razor"
+#line 11 "C:\xampp\htdocs\PEMROGRAMANFRONTEND\test\Tugas 5\Employees\_Imports.razor"
 using EmployeeManagement.Models;
 
 #line default
